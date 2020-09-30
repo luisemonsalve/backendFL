5a0c102dec67169996d6dedf56354493cd4897dc
explora <luis.monsalve@parqueexplora.org>
2020-09-29 19:43:04 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require('./server/app');
+
+
+port = 3000
+
+async function start() {
+    app.listen(port, () => {
+        console.log(`Listen on port ${port}`)
+    })
+}
+
+
+start();
